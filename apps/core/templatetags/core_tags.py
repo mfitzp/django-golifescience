ed4054bc4605967d3f5b687baa6d43dd2274444d
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2013-01-22 14:29:32 +0000

    Remove time words with s first; or we end up with trailing s

--- apps/core/templatetags/core_tags.py
+++ apps/core/templatetags/core_tags.py
@@ -136,8 +136,8 @@ def verybrieftimesince(value, arg=None):
         t = brieftimesince(value)
         # Substitute the words for d, h, m, s
         sdict = {
-            'year':'y', 'month': 'm', 'day':'d', 'hour':'h', 'minute':'m', 'second':'s',
             'years':'y', 'months': 'm', 'days':'d', 'hours':'h', 'minutes':'m', 'seconds':'s'
+            'year':'y', 'month': 'm', 'day':'d', 'hour':'h', 'minute':'m', 'second':'s',
             }
         t = reduce(lambda x, y: x.replace(y, sdict[y]), sdict, t)
         return t.replace(" ", "")
