13e280ecc52f7435ccfc35ea03763667c9c86b8f
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2013-01-22 16:43:10 +0000

    Add widget disabled message

--- urls/do.py
+++ urls/do.py
@@ -4,5 +4,7 @@ urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
     (r'^methods/',      include('methods.urls')),
     (r'^ajax/',      include('ajax.urls')),
+
+    (r'widgets/wordpress/methods/',  'django.views.generic.simple.direct_to_template', {'template': 'widgets/disabled.html'}),
 )
 
--- urls/install.py
+++ urls/install.py
@@ -4,5 +4,7 @@ urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
     (r'^software/',      include('applications.urls')),
     (r'^ajax/',      include('ajax.urls')),
+
+    (r'widgets/wordpress/methods/',  'django.views.generic.simple.direct_to_template', {'template': 'widgets/disabled.html'}),
 )
 
