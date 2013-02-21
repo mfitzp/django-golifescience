8977f98c523233324343fac15ef6001cc962238e
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2013-02-21 23:27:17 +0000

    Fix up twitter feed

--- apps/references/search_indexes.py
+++ apps/references/search_indexes.py
@@ -12,5 +12,4 @@ class ReferenceIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
-
 '''
