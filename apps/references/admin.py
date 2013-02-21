f39391f40419e7a958fef4bf11d9cba53f4e2844
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2013-02-21 22:56:46 +0000

    Ref management

--- apps/references/admin.py
+++ apps/references/admin.py
@@ -4,7 +4,7 @@ from references.models import *
 
 class ReferenceAdmin(admin.ModelAdmin):
     fields = ('uri','namespace', 'title', 'description', 'author', 'publisher', 'published',)
-    readonly_fields =('namespace', 'title', 'description', 'author', 'publisher', 'published',)
+    #readonly_fields =('namespace', 'title', 'description', 'author', 'publisher', 'published',)
 
 
 admin.site.register(Reference, ReferenceAdmin)
