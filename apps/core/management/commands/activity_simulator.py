582a25d569977e7957ce0486de27fbe60eb6bea0
Martin Fitzpatrick <mfitzp@abl.es>
2013-03-08 17:15:46 +0000

    Object activity feed simulation; non-destructive (doh)

--- apps/core/management/commands/activity_simulator.py
+++ apps/core/management/commands/activity_simulator.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
         actions = [object_saved]
         tfs = [ True, False ]
 
-        objt = choice(obj)
+        objt = choice(objs)
         action = choice(actions)
         tf = choice(tfs)        
 
