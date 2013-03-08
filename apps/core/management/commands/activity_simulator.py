d886dc2fe0fa11e7e40e0cf9d6c6bbdb0c050b02
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2013-03-08 00:23:05 +0000

    Activity tweaks; not uid=0 (can do those activities myself)

--- apps/core/management/commands/activity_simulator.py
+++ apps/core/management/commands/activity_simulator.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         objs.extend( [Application])
         objs.extend( [Method] * 20 ) # Compensate for relative numbers of each
         
-        obj = choice(objs).objects.all().order_by('?')[0]
+        obj = choice(objs).objects.exclude(created_by__id=1).order_by('?')[0]
 
         print "Simulating edit to %s" % obj
 
