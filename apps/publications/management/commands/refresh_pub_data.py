2ede5608b180d2b7d92b45018018acd16f45b897
Martin Fitzpatrick <mfitzp@abl.es>
2013-03-18 22:54:18 +0000

    Repopulating objects;

---
+++ apps/publications/management/commands/refresh_pub_data.py
@@ -0,0 +1,40 @@
+# coding=UTF-8
+from optparse import make_option
+import sys
+import datetime, string
+import urllib, re
+from urlparse import urlparse
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
+# methodmint
+from publications.models import Publication
+
+class Command(BaseCommand):
+    help = "Refresh data for publication"
+
+    option_list = BaseCommand.option_list + (
+        make_option('--start',
+            action='store_int',
+            dest='start_id',
+            default=1,
+            help='Start from id'),
+        make_option('--end',
+            action='store_int',
+            dest='end_id',
+            default=99999999,
+            help='End at id'),
+        )
+
+    def handle(self, *args, **options):
+    
+        refs = Reference.objects.filter(id__ge=options['start_id'], id__le=options['end_id'])
+        
+        for ref in refs:
+            print "Query for - %s" % ref
+            ref.autopopulate()
+            
+
+
